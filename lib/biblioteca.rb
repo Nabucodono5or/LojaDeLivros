a959fc512189fa321d43e442bbdc1843ef140931
jefferson abreu <nabucodonosor310@gmail.com>
2015-08-20 22:19:58 -0300

    alterado bloco para objeto proc

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -15,9 +15,9 @@ class Biblioteca
 		@livros.values.flatten
 	end	
 		
-	def livros_por_categoria(categoria, &bloco)
+	def livros_por_categoria(categoria)
 		@livros[categoria].each do |livro|
-		bloco.call livro
+			yield livro if block_given?
 		end
 	end	
 end	
