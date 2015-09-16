b52df91eef9499978d4a4961ab94a18d65135f8c
jefferson abreu <nabucodonosor310@gmail.com>
2015-09-15 22:00:36 -0300

    corrigindo crud

--- lib/revista.rb
+++ lib/revista.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
+ require 'fileutils'
 class Revista
 	
-	attr_reader :titulo, :id
+	attr_reader :titulo, :id, :destroy, :new_record
 	attr_accessor :valor #para permitir escrita no atributo valor
 	
 	
@@ -9,19 +10,29 @@ class Revista
 		@titulo = titulo
 		@valor = valor
 		@id = self.class.next_id #gerará um id automatico do método next_id
+		@destroyed = false
+		@new_record = true
 	end	
 	
+	
 	def save
+		@new_record = false
 		File.open("db/revistas/#{@id}.yaml", "w") do |file|
 			file.puts serialize
 		end	
 	
 	end	
 	
+	def destroy
+		unless @destroyed or @new_record
+			@destroyed = true
+			FileUtils.rm "db/revistas/#{@id}.yaml"
+		end	
+	end
+
+
 	def self.find(id)
-		raise DocumentNotFound,"Arquivo db/revistas/#{id} não encontrado.",caller
-			unless File.exists?("db/revistas/#{id}.yaml")
-		end		
+		raise DocumentNotFound,"Arquivo db/revistas/#{id} não encontrado.",caller unless File.exists?("db/revistas/#{id}.yaml")
 		YAML.load File.open("db/revistas/#{id}.yaml","r")
 	end	
 	
